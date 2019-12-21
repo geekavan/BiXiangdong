fd1f908aa9461535e4f668afc1df924cff13d5cd
Ivan <2937923559@qq.com>
2019-12-21 11:57:20 +0800

    手推车与篮子的例子

--- Demo21_09.java
+++ Demo21_09.java
@@ -3,6 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 class Demo21_09{
+    private static final int BUFFER_SIZE = 4096;
     public static void main(String[] args){
         FileReader fr = null;
         FileWriter fw = null;
@@ -10,20 +11,17 @@ class Demo21_09{
             fr = new FileReader("e:/FileWriter.txt");
             fw = new FileWriter("f:/就叫这个名字了.txt");
 
-            char[] ch = new char[4096];
+            char[] ch = new char[BUFFER_SIZE];
+            int len = 0;
 
-            while(fr.read(ch)!=-1){
-                fw.write(ch);
+            while((len=fr.read(ch))!=-1){
+                fw.write(ch, 0, len);
             }
         }
         catch(IOException e){}
         finally{
-            try{
-            if(fr!=null) fr.close();
-            if(fw!=null) fw.close();
-            }
-            catch(IOException e){}
-            finally{}
+            try{if(fr!=null) fr.close();}catch(IOException e){throw new RuntimeException("读取流关闭异常");}
+            try{if(fw!=null) fw.close();}catch(IOException e){throw new RuntimeException("写入流关闭异常");}
         }
     }
 }
